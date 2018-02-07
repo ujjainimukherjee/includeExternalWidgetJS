a9ec84688c8a6c5bc0de37ff69401de2fc0836f2
Ujjaini Mukherjee <umukherjee@ancestry.com>
2018-02-07 11:18:11 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import {Widget} from './widget';
+
 import { Component } from '@angular/core';
 
 @Component({
---
+++ src/app/widget.js
@@ -0,0 +1,14 @@
+// import {Base} from './base';
+
+
+export class Widget {
+    constructor(){
+        // super();
+      console.log('Hello, I am a widget');
+        // const widget = document.getElementsByClassName(element)[0];
+        // widget.querySelector('#main').innerHTML = 'UJJAINI TESTING';
+        // show 'Hello, I am a widget' inside the widget DOM
+    }
+}
+
+
